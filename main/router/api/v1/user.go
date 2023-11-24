80b666924b9d2d5e37d45471978c79c3831ce6cc
Nachrichtendienst <lennart.meyer.2022@leibniz-fh.de>
2023-11-24 13:03:50 +0100

    login function
    - added login function

--- main/router/api/v1/user.go
+++ main/router/api/v1/user.go
@@ -2,12 +2,52 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"src/main/crypt"
+	"src/main/database"
+	"src/main/database/models"
 )
 
 func userHandler(cg *gin.RouterGroup) {
 	//    /api/v1/usr/login
-	cg.GET("/login", func(c *gin.Context) {
+	cg.POST("/login", func(c *gin.Context) {
+		//check body for username and password
+		var requestBody struct {
+			Username string `json:"username" binding:"required"`
+			Password string `json:"password" binding:"required"`
+		}
 
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		username := requestBody.Username
+		password := requestBody.Password
+
+		//check if user exists
+		var user models.User
+		err := database.MongoDB.Collection("user").FindOne(c, bson.M{"username": username}).Decode(&user)
+		//if user exists, check password using crypt
+		if err == nil {
+			if crypt.CheckPasswordHash(password, user.Password) {
+				//generate jwt token
+				token, err := crypt.GenerateLoginToken(user.ID)
+				if err != nil {
+					c.JSON(500, gin.H{"message": "Internal server error"})
+					return
+				}
+
+				//set cookie with age of 2 days, setting maxAge to: 3600 * 24 * 2
+				c.SetCookie("auth", token, 3600*24*2, "/", "", false, false)
+
+				c.JSON(200, gin.H{"status": 200, "message": "Logged in"})
+			} else {
+				c.JSON(401, gin.H{"message": "Unauthorized"})
+			}
+		} else {
+			c.JSON(401, gin.H{"message": "Unauthorized"})
+		}
 	})
 
 	cg.POST("/register", func(c *gin.Context) {
