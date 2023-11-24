b96671cddd7bb02eca1085a4ba4969c4fd66f5fb
Leon <leon.winands.2022@leibniz-fh.de>
2023-11-24 13:54:54 +0100

    Updated DB Models
    added missing Id in the following db models:
    -LectureGroup
    -StudentGroup
    SemesterGroup
    -TimeTable

--- main/database/models/StudentGroup.go
+++ main/database/models/StudentGroup.go
@@ -3,6 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type StudentGroup struct {
+	ID              primitive.ObjectID   `json:"id" bson:"_id"`
 	Name            string               `json:"name" bson:"name"`
 	LectureGroupIds []primitive.ObjectID `json:"lectureGroupIds" bson:"lectureGroupIds"`
 	TimeTableID     primitive.ObjectID   `json:"timeTableId" bson:"timeTableId"`
