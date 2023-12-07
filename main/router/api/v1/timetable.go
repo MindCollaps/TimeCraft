46e69915a3f86fee12eae7d7e805ca2a21b881c5
Louis1505 <louistrenkner@t-online.de>
2023-12-07 12:59:40 +0100

    created timetable "creation"

--- main/router/api/v1/timetable.go
+++ main/router/api/v1/timetable.go
@@ -1,7 +1,15 @@
 package v1
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
+	"src/main/database"
+	"src/main/database/models"
 )
 
 func tblHandler(cg *gin.RouterGroup) {
@@ -9,9 +17,9 @@ func tblHandler(cg *gin.RouterGroup) {
 	cg.POST("/create", func(c *gin.Context) {
 		//check body for username and password
 		var requestBody struct {
-			Id   string `json:"id" binding:"required"`
-			Name string `json:"name" binding:"required"`
-			Days string `json:"days" binding:"required"`
+			Id   primitive.ObjectID   `json:"id" binding:"required"`
+			Name string               `json:"name" binding:"required"`
+			Days []primitive.ObjectID `json:"days" binding:"required"`
 		}
 
 		if err := c.ShouldBindJSON(&requestBody); err != nil {
@@ -19,11 +27,33 @@ func tblHandler(cg *gin.RouterGroup) {
 			return
 		}
 
-		id := requestBody.Id
 		name := requestBody.Name
 		days := requestBody.Days
 
-		var existingTbl models.T
+		var existingTbl models.TimeTable
+		err := database.MongoDB.Collection("TimeTable").FindOne(c, bson.M{"Name": name}).Decode(&existingTbl)
+
+		if err == nil {
+			// Table with the same name already exists
+			c.JSON(http.StatusConflict, gin.H{"message": "TimeTable already exists"})
+			fmt.Println("TimeTable already exists")
+			return
+		} else if err != mongo.ErrNoDocuments {
+			// Handle other database query errors
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Database error"})
+			fmt.Println(err)
+			return
+		}
+
+		newTable := models.TimeTable{
+			ID:   primitive.NewObjectID(),
+			Name: name,
+			Days: days,
+		}
+
+		database.MongoDB.Collection("TimeTable").InsertOne(c, newTable, options.InsertOne())
+		c.JSON(http.StatusOK, gin.H{"status": 200, "message": "Created Timetable"})
+
 	})
 
 }
