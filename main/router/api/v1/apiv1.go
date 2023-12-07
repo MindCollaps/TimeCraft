7f2a9116eccb702be348666bb93c9c6c43685d83
Louis1505 <louistrenkner@t-online.de>
2023-12-07 12:36:14 +0100

    Changed handler function

--- main/router/api/v1/apiv1.go
+++ main/router/api/v1/apiv1.go
@@ -4,12 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func Handler(rg *gin.RouterGroup) {
 	user := rg.Group("/usr")
-
-	userHandler(user)
-}
-
-func Handler2(rg *gin.RouterGroup) {
 	timetable := rg.Group("/tbl")
-
+	userHandler(user)
 	tblHandler(timetable)
 }
--- main/router/api/v1/timetable.go
+++ main/router/api/v1/timetable.go
@@ -1,7 +1,29 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func tblHandler(cg *gin.RouterGroup) {
 	//    /api/v1/tbl/...
+	cg.POST("/create", func(c *gin.Context) {
+		//check body for username and password
+		var requestBody struct {
+			Id   string `json:"id" binding:"required"`
+			Name string `json:"name" binding:"required"`
+			Days string `json:"days" binding:"required"`
+		}
+
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		id := requestBody.Id
+		name := requestBody.Name
+		days := requestBody.Days
+
+		var existingTbl models.T
+	})
+
 }
