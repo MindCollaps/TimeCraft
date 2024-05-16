96f5efc6b7160add65587c472113f7954b2dedd1
CrazyTim71 <tim.niess.2022@leibniz-fh.de>
2024-05-16 16:11:25 +0200

    work on ical

--- main/router/api/v1/ical.go
+++ main/router/api/v1/ical.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"bytes"
 	"errors"
 	"github.com/arran4/golang-ical"
 	"github.com/gin-gonic/gin"
@@ -177,6 +178,118 @@ func icalHandler(cg *gin.RouterGroup) {
 		}
 
 		c.Data(http.StatusOK, "text/calendar", []byte(icalEntry.Text))
+	})
+
+	cg.PATCH("/:id", func(c *gin.Context) {
+		id, err := primitive.ObjectIDFromHex(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": "An error occurred", "error": "Invalid ID"})
+			log.Println(err)
+			return
+		}
+
+		var icalEntry models.IcalEntry
+		err = database.MongoDB.Collection("IcalEntry").FindOne(c, bson.M{"_id": id}).Decode(&icalEntry)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"msg": "An error occurred", "error": "iCal not found"})
+			log.Println(err)
+			return
+
+		}
+
+		var timetable models.TimeTable
+		err = database.MongoDB.Collection("TimeTable").FindOne(c, bson.M{"_id": icalEntry.TimeTableId}).Decode(&timetable)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"msg": "An error occurred", "error": "TimeTable not found"})
+			log.Println(err)
+			return
+
+		}
+
+		var requestBody struct {
+			Name        string             `json:"name"`
+			Text        string             `json:"text"`
+			TimeTableId primitive.ObjectID `json:"timeTableId"`
+			LastUpdated primitive.DateTime `json:"lastUpdated"`
+		}
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": "An error occurred", "error": "Invalid body"})
+			log.Println(err)
+			return
+		}
+
+		update := bson.M{}
+		if requestBody.Name != "" {
+			update["name"] = requestBody.Name
+		}
+
+		if requestBody.Text != "" {
+			// check if the text is a valid ics stream
+			reader := bytes.NewReader([]byte(requestBody.Text))
+			_, err := ics.ParseCalendar(reader)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"msg": "Invalid iCal text", "error": "The provided text can't be parsed as ical"})
+				log.Println(err)
+				return
+			}
+			update["text"] = requestBody.Text
+		}
+
+		if requestBody.TimeTableId != primitive.NilObjectID {
+			if err := database.MongoDB.Collection("TimeTable").FindOne(c, bson.M{"_id": requestBody.TimeTableId}).Decode(&timetable); err != nil {
+				c.JSON(http.StatusNotFound, gin.H{"msg": "An error occurred", "error": "TimeTable not found"})
+				log.Println(err)
+				return
+			}
+			update["timeTableId"] = requestBody.TimeTableId
+		}
+
+		if requestBody.LastUpdated != primitive.DateTime(0) {
+			providedTime := requestBody.LastUpdated.Time()
+			if providedTime.After(time.Now()) || providedTime.Equal(time.Now()) {
+				update["lastUpdated"] = requestBody.LastUpdated
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{"msg": "An error occurred", "error": "Invalid lastUpdated"})
+				return
+			}
+		}
+
+		result, err := database.MongoDB.Collection("IcalEntry").UpdateOne(c, bson.M{"_id": id}, bson.M{"$set": update})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": "An error occurred", "error": "Database error"})
+			log.Println(err)
+			return
+		}
+		if result.ModifiedCount == 0 {
+			c.JSON(http.StatusNotModified, gin.H{"msg": "Nothing was updated", "error": "No data provided to update"})
+			log.Println("Warning: No data provided to update the IcalEntry")
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"msg": "iCal updated successfully"})
+	})
+
+	cg.DELETE("/:id", func(c *gin.Context) {
+		id, err := primitive.ObjectIDFromHex(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": "An error occurred", "error": "Invalid ID"})
+			log.Println(err)
+			return
+		}
+
+		result, err := database.MongoDB.Collection("IcalEntry").DeleteOne(c, bson.M{"_id": id})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": "An error occurred", "error": "Database error"})
+			log.Println(err)
+			return
+		}
+
+		if result.DeletedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"msg": "An error occurred", "error": "iCal not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"msg": "iCal deleted successfully"})
 	})
 
 }
